b6d0849b76566ae024b7ee1dd7f23382c8b831d9
LT <ltouretv@gmail.com>
2020-05-23 20:11:54 +0200

    fixed bugged itoa for in min

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,46 +6,62 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 18:48:36 by ltouret           #+#    #+#             */
-/*   Updated: 2020/02/26 16:39:53 by ltouret          ###   ########.fr       */
+/*   Updated: 2020/05/22 21:43:56 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_len(int nb)
+static unsigned int	get_nb_digit(long n_l, int sign)
 {
-	int		i;
+	unsigned int	nb_digit;
 
-	i = (0 >= nb) ? 1 : 0;
-	while (nb != 0)
+	if (n_l == 0)
+		return (1);
+	nb_digit = 0;
+	while (n_l > 0)
 	{
-		nb /= 10;
-		i++;
+		n_l /= 10;
+		nb_digit++;
 	}
-	return (i);
+	if (sign == -1)
+		nb_digit++;
+	return (nb_digit);
 }
 
-char	*ft_itoa(int n)
+static void			convert_nb(char *outstr, long n_l, unsigned int nb_digit,
+		int sign)
 {
-	int		i;
-	int		temp;
-	char	*str;
-
-	i = ft_len(n);
-	temp = n;
-	if ((str = (char*)malloc((i + 1) * sizeof(char))))
+	outstr[nb_digit] = '\0';
+	outstr[--nb_digit] = n_l % 10 + '0';
+	n_l /= 10;
+	while (n_l > 0)
 	{
-		str[i--] = '\0';
-		if (0 > temp)
-			str[0] = '-';
-		else if (temp == 0)
-			str[0] = '0';
-		while (temp != 0)
-		{
-			str[i] = (temp % 10) > 0 ? (temp % 10) + 48 : (-temp % 10) + 48;
-			temp /= 10;
-			i--;
-		}
+		outstr[--nb_digit] = n_l % 10 + '0';
+		n_l /= 10;
 	}
-	return (str);
+	if (sign == -1)
+		outstr[0] = '-';
+}
+
+char				*ft_itoa(int n)
+{
+	char			*outstr;
+	long			n_l;
+	unsigned int	nb_digit;
+	int				sign;
+
+	sign = 1;
+	if (n < 0)
+	{
+		n_l = (long)n * -1;
+		sign = -1;
+	}
+	else
+		n_l = n;
+	nb_digit = get_nb_digit(n_l, sign);
+	if (!(outstr = malloc(sizeof(char) * nb_digit + 1)))
+		return (NULL);
+	convert_nb(outstr, n_l, nb_digit, sign);
+	return (outstr);
 }
